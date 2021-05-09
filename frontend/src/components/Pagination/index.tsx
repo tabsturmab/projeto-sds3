f1f764e57bf55330d84cbf72609026e7a6447eac
Andre Silva <tadsturmab2014@gmail.com>
2021-05-09 18:47:48 -0300

    Correction requests

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 const Pagination = ( { page, onPageChange } : Props) => {
     return (
-        <div className="row d-flex justify-content-center">
+        <div className="row d-flex">
             <nav>
-                <ul className="pagination">
+                <ul className="pagination justify-content-center">
                     <li className={`page-item ${page.first ? 'disabled' : ''}`}>
                         <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
                     </li>
---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-andresilva.herokuapp.com';
